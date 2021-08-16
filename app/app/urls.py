e7254afc17672e89166bb472fb5518555377b695
Hans Heller <h.heller.zagal@gmail.com>
2021-08-16 11:54:37 -0400

    Se modifica opcion delete por inactivar

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
 
     path('',include(('bases.urls','bases'), namespace='bases')),
-    #path('inv/',include(('inv.urls','inv'), namespace='inv')),
+    path('inv/',include(('inv.urls','inv'), namespace='inv')),
     #path('cmp/', include(('cmp.urls', 'cmp'), namespace='cmp')),
     path('admin/', admin.site.urls),
 ]
