09b9f3a47cadb6239243ded054e35cd4ce1ee4d1
Hans Heller <h.heller.zagal@gmail.com>
2021-08-19 19:59:14 -0400

    Se crea ClaseModelo2 para gestionar usuarios y tablas gestion clientes

--- app/app/settings.py
+++ app/app/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'inv',
     'cmp',
     'fac',
+    'django_userforeignkey',
+
 ]
 
 MIDDLEWARE = [
@@ -53,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_userforeignkey.middleware.UserForeignKeyMiddleware',
+
 ]
 
 ROOT_URLCONF = 'app.urls'
--- app/bases/models.py
+++ app/bases/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from django.contrib.auth.models import User
-
+from django_userforeignkey.models.fields import UserForeignKey
 # Create your models here.
 
 
@@ -11,6 +11,18 @@ class ClaseModelo(models.Model):
     fm = models.DateTimeField(auto_now=True)
     uc = models.ForeignKey(User, on_delete=models.CASCADE)
     um = models.IntegerField(blank=True,null=True)
+
+    class Meta:
+        abstract=True
+        
+class ClaseModelo2(models.Model):
+    estado = models.BooleanField(default=True)
+    fc = models.DateTimeField(auto_now_add=True)
+    fm = models.DateTimeField(auto_now=True)
+    #uc = models.ForeignKey(User, on_delete=models.CASCADE)
+    #um = models.IntegerField(blank=True,null=True)
+    uc= UserForeignKey(auto_user_add=True, related_name='+')
+    um= UserForeignKey(auto_user=True, related_name='+')
 
     class Meta:
         abstract=True
